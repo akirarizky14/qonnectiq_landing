b9550355023a3a5def90461115d0d96c8276c844
akirarizky14 <akirarizkyyy@gmail.com>
2024-02-18 11:24:51 +0300

    feat: Restructure project directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './views/Home/Home'
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
